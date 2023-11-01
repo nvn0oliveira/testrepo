c01a99ee6a973574970983561a5f49fe6872c44e
Nuno Oliveira <nuno.alm.oliveira@gmail.com>
2023-11-01 16:28:57 +0000

    cpp file

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/*
+- run code from dev terminal from visual studio
+- run cl.exe main.cpp
+- .\main.exe
+*/
+
+int main() {
+    std::cout << "Hello World!";
+    int a = 2;
+    std::cout << a << std::endl;
+    return 0;
+}
